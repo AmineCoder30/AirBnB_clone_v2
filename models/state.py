14b3cca3f76462329906dcaa1dc6ae938d39158b
AmineCoder30 <code30a@gmail.com>
2023-09-19 15:37:07 -0700

    and if statemeent for the type storage

--- models/state.py
+++ models/state.py
@@ -4,20 +4,29 @@ from models.base_model import BaseModel, Base
 from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
-class State(BaseModel):
-    """ State class """
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state',
-                              cascade='all, delete, delete-orphan')
-    @property
-    def cities(self):
-        '''returns the list of City instances
-        '''
-        from models import storage
-        related_cities = []
-        cities = storage.all(City)
-        for city in cities.values():
-            if city.state_id == self.id:
-                related_cities.append(city)
-        return related_cities
+storage_type = getenv("HBNB_TYPE_STORAGE")
+
+class State(BaseModel, Base):
+    """ State class"""
+    __tablename__ = 'states'
+    if storage_type == "db":
+        name = Column(String(128), nullable=False, unique=True)
+        cities = relationship('City', cascade="all,delete", backref="state")
+    else:
+        name = ""
+
+
+        @property
+        def cities(self):
+            '''returns the list of City instances with
+            '''
+            from models import storage
+            related_cities = []
+            cities = storage.all(City)
+            for city in cities.values():
+                if city.state_id == self.id:
+                    related_cities.append(city)
+            return related_cities
+
