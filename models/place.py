d0dc1fbdc17fb7f1d0d3cb1015bcbcdd6efced05
AmineCoder30 <code30a@gmail.com>
2023-09-20 02:52:40 -0700

    update base model

--- models/place.py
+++ models/place.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 
-from models.amenity  import Amenity
+from models.amenity import Amenity
 from models.review import Review
 from models.base_model import BaseModel, Base
+from models import storage_type
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.sql.schema import Table
 from sqlalchemy.orm import relationship
-from os import getenv
 
-storage_type = getenv("HBNB_TYPE_STORAGE")
 
 if storage_type == 'db':
     place_amenity = Table('place_amenity', Base.metadata,
                           Column('place_id', String(60),
                                  ForeignKey('places.id'),
-                                 primary_key=True,
-                                 nullable=False),
+                                 primary_key=True, nullable=False),
                           Column('amenity_id', String(60),
                                  ForeignKey('amenities.id'),
-                                 primary_key=True,
-                                 nullable=False)
+                                 primary_key=True, nullable=False)
                           )
 
 
@@ -28,8 +25,8 @@ class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
     if storage_type == 'db':
-        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
@@ -38,10 +35,10 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        reviews = relationship('Review', backref='place',
-                               cascade='all, delete, delete-orphan')
-        amenities = relationship('Amenity', secondary=place_amenity,
-                                 viewonly=False, backref='place_amenities')
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False,
+                                 back_populates="place_amenities")
+        reviews = relationship('Review', cascade="all,delete", backref="place")
     else:
         city_id = ""
         user_id = ""
